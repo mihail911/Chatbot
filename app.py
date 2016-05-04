f230ebe5c77002c67e6792c8e970fe2852eaa07c
Mihail Eric <meric@eliza-snlp.stanford.edu>
2016-05-03 23:54:29 -0700

    printing contents of request

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
+    print "hi"
+    print request
+    print request.data
+    print request.args
+    print request.form 
     return 'Hello World! asf\n'
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1")
+    app.run(host="0.0.0.0")
